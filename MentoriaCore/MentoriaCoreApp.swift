ebc53b1ca79045b51a2a29b03e7c8c2d276c7f7e
Makoto <rmt.makoto@gmail.com>
2026-06-21 19:59:43 -0300

    primeiro commit

---
+++ MentoriaCore/MentoriaCoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentoriaCoreApp.swift
+//  MentoriaCore
+//
+//  Created by Rodrigo Takumi on 21/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MentoriaCoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
